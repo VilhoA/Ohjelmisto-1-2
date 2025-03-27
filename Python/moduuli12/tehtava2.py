bfe211cd1ed0bd1f64df1461ed9d9c50b4445960
Ville <ville.ahola@protonmail.com>
2025-03-27 19:09:02 +0200

    moduuli 12 valmis

--- Python/moduuli12/tehtava2.py
+++ Python/moduuli12/tehtava2.py
@@ -1,16 +1,16 @@
 import requests
 import json
 
-city = input('Syötä kaupunki jonka säätiedot haluat: ')
-appid = input('syötä API avain: ')
+city = input('What city would you like the weather for?\nCity: ')
+appid = input('openweather API key: ')
 url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}&units=metric'
 
 #print(url)
 
 tulos = requests.get(url).json()
-print(json.dumps(tulos, indent=4))
+#print(json.dumps(tulos, indent=4))
 kuvaus=(json.dumps(tulos['weather'][0]['description']))
 asteetc=(json.dumps(tulos['main']['temp']))
 
-print(f'Sää kohteessa on {asteetc} astetta ja {kuvaus}')
-print(f'The weather is {asteetc} degrees celcius and {str(kuvaus)}')
+#print(f'Sää kohteessa on {asteetc} astetta ja {kuvaus}')
+print(f'The weather is {asteetc} degrees celcius and the description is {kuvaus}')
