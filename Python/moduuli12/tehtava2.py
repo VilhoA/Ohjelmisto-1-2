6a0c2cb370ffe7a13525886a7617fff3e05535cd
Ville <ville.ahola@protonmail.com>
2025-03-27 15:52:15 +0200

    sää api toimii nyt suomen kaupungeilla ja unitit on metric

--- Python/moduuli12/tehtava2.py
+++ Python/moduuli12/tehtava2.py
@@ -1,13 +1,11 @@
 import requests
 import json
 
-lat = 60.6
-lon = 24.8
-part = ""
+city = 'espoo'
 appid = "65509b5468f117ae78d69dcef210c80e"
-url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&exclude={part}&appid={appid}'
+url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}&units=metric'
 
-print(url)
+#print(url)
 
 tulos = requests.get(url).json()
 print(json.dumps(tulos, indent=4))
