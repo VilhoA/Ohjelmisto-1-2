cdb5cb8d15b611dca0e75def17fb032c107f51c5
Ville <ville.ahola@protonmail.com>
2025-03-27 16:35:20 +0200

    muutettu api avain käyttäjän syötettäväksi

--- Python/moduuli12/tehtava2.py
+++ Python/moduuli12/tehtava2.py
@@ -1,11 +1,16 @@
 import requests
 import json
 
-city = 'espoo'
-appid = "65509b5468f117ae78d69dcef210c80e"
+city = input('Syötä kaupunki jonka säätiedot haluat: ')
+appid = input('syötä API avain: ')
 url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}&units=metric'
 
 #print(url)
 
 tulos = requests.get(url).json()
 print(json.dumps(tulos, indent=4))
+kuvaus=(json.dumps(tulos['weather'][0]['description']))
+asteetc=(json.dumps(tulos['main']['temp']))
+
+print(f'Sää kohteessa on {asteetc} astetta ja {kuvaus}')
+print(f'The weather is {asteetc} degrees celcius and {str(kuvaus)}')
